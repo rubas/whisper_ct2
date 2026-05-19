9afa2c58d1ea6ca0e86d6cf18a05a5714e76f109
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-05-19 10:32:01 +0200

    chore: update NIF checksum for v0.3.1

--- checksum-Elixir.WhisperCt2.Native.exs
+++ checksum-Elixir.WhisperCt2.Native.exs
@@ -1,1 +1,6 @@
-%{}
+%{
+  "libwhisper_ct2_native-v0.3.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:49a446cce1914bb407fbcc74d3996e9422f18b076d839e2b90feff5969ccb41f",
+  "libwhisper_ct2_native-v0.3.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7fe292122b81099f7c2ac8a751b66f38c87cfdb62b3aa0f0e1ef1c0de1a2382c",
+  "libwhisper_ct2_native-v0.3.1-nif-2.17-x86_64-unknown-linux-gnu--mkl.so.tar.gz" => "sha256:1954142295bf2949f708ec36ac658b12c28a702a121bfc1d88c5bd132ee3c10a",
+  "libwhisper_ct2_native-v0.3.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:2c3ac945d91f72d3d60f40c59be2348a4052a4afa59c2ca7206d783f56d7abac",
+}
