1c1530b31ae5b5589e895a3ad61347b08ce39d13
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-05-20 14:39:01 +0200

    chore: add NIF checksums for v0.5.0
    Regenerated checksum-Elixir.WhisperCt2.Native.exs from the published
    v0.5.0 release artefacts. Also guard HEAD^ in the release workflow's
    detect step so a parentless root commit does not abort it.

--- checksum-Elixir.WhisperCt2.Native.exs
+++ checksum-Elixir.WhisperCt2.Native.exs
@@ -1,10 +1,6 @@
 %{
-  "libwhisper_ct2_native-v0.4.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" =>
-    "sha256:8c8f065238aff514326d00da851905f9f1284db2a5942692d9a3559aba541597",
-  "libwhisper_ct2_native-v0.4.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:5bbe4503d62526fe1080a682f1c52a5ee4c308615c3d12118b8a988c004f44b0",
-  "libwhisper_ct2_native-v0.4.1-nif-2.17-x86_64-unknown-linux-gnu--mkl.so.tar.gz" =>
-    "sha256:df31a463abb7ad18f862d3a066074cde4e63078ff860149bb8a883d47934be27",
-  "libwhisper_ct2_native-v0.4.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:78d20b90c84636a1140b3e65c97a61f890b4603ae3ae3eb59a94c6cfde31cafc"
+  "libwhisper_ct2_native-v0.5.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:6d68775b6d7ae0faeba82568174a3d746136e0106469ecebd1599b3fab10c98e",
+  "libwhisper_ct2_native-v0.5.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:88199cb84339c9e56d33467c7743d1a156a1f799e79473ef98fb2ef5e733f20a",
+  "libwhisper_ct2_native-v0.5.0-nif-2.17-x86_64-unknown-linux-gnu--mkl.so.tar.gz" => "sha256:94bb516678854087144d0dcdc0509dbc4559c1185e08e55d7ba586b1405cbf9c",
+  "libwhisper_ct2_native-v0.5.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:8c522ae3a27a994e0a0cddde8c097daaf26570d72dbfc5349179650d3cf1ee5b",
 }
