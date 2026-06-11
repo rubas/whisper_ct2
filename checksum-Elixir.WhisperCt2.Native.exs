8fa70c6cbc0d3b8dff0dd19a249bf1e1040f381d
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-06-11 17:52:36 +0200

    chore: update NIF checksum for v0.6.1

--- checksum-Elixir.WhisperCt2.Native.exs
+++ checksum-Elixir.WhisperCt2.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libwhisper_ct2_native-v0.6.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:adee852ead3465a41ba3cc64c02a0636080c9354e0dab30e1bbee1cba7954546",
-  "libwhisper_ct2_native-v0.6.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:0d2817ceef46170d51bfd3342cb26e5913cdc3a910a744452dfa80b6104036c0",
-  "libwhisper_ct2_native-v0.6.0-nif-2.17-x86_64-unknown-linux-gnu--mkl.so.tar.gz" => "sha256:671e76357e508f800c65be501e61faf5d32a5198db22be61e5c2d2d132139c28",
-  "libwhisper_ct2_native-v0.6.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:98d9502f3b9594501a6d43093efc51869f9b11e21267e2bc463e4137277d0ebe",
+  "libwhisper_ct2_native-v0.6.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:998fd7ad47f6ebe8977e7dd549e2a20f6925e8d1f10e15085ea3248b760bac2b",
+  "libwhisper_ct2_native-v0.6.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:838776afc91ad9e8b5910f9ae1f61613f6340a603927011f9b0a98ae5bda5dd4",
+  "libwhisper_ct2_native-v0.6.1-nif-2.17-x86_64-unknown-linux-gnu--mkl.so.tar.gz" => "sha256:a6bb97cbf736c2a8462dd601a9b0c8e6b21fcac98aa092288dd2d79be0ee8e60",
+  "libwhisper_ct2_native-v0.6.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:056c439a73c7ce8e34ae38a75ba83a62b16f11ee1f1054820386efd60912572e",
 }
